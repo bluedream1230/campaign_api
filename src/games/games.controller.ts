562c37fe976e04a462142f010fb2758cf99e05b4
bluedream1230 <bluedream921230@gmail.com>
2022-12-11 15:08:19 -0500

    update: unauthorized game

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -22,7 +22,6 @@ import CreateEventDto from "src/events/dto/createEvent.dto";
 import GamesService from "./games.service";
 import CreateGameDto from "./dto/createGame.dto";
 
-@ApiBearerAuth()
 @ApiTags("Games")
 @Controller("games")
 @UseInterceptors(ClassSerializerInterceptor)
@@ -30,14 +29,12 @@ export default class GamesController {
   constructor(private readonly gamesService: GamesService) {}
 
   @Get()
-  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: "Get all games" })
   getAllGames() {
     return this.gamesService.getAllGames();
   }
 
   @Get(":id")
-  @UseGuards(JwtAuthGuard)
   @ApiResponse({
     status: 200,
     description: "The found record",
@@ -47,7 +44,6 @@ export default class GamesController {
   }
 
   @Post()
-  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: "Create game" })
   @ApiResponse({ status: 403, description: "Forbidden." })
   async createGame(@Body() game: CreateGameDto) {
@@ -55,7 +51,6 @@ export default class GamesController {
   }
 
   @Delete(":id")
-  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: "Delete game" })
   async deleteGame(@Param() { id }: FindOneParams) {
     return this.gamesService.deleteGame(Number(id));
